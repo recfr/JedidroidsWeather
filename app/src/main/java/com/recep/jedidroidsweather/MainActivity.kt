a45b2dd66275b53a43bf68025d3c6d3ba6da260d
recepfirincioglu <recep.firincioglu@gmail.com>
2021-08-23 23:46:32 +0300

    Detail info added

--- app/src/main/java/com/recep/jedidroidsweather/MainActivity.kt
+++ app/src/main/java/com/recep/jedidroidsweather/MainActivity.kt
@@ -1,14 +1,18 @@
 package com.recep.jedidroidsweather
 
-import android.graphics.Color
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.View
-import android.view.WindowManager
+import androidx.appcompat.app.AppCompatActivity
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
     }
+
+    //TODO Get date info.
+    //TODO cast ProgressBar value and set color via actual value
+
+
 }
